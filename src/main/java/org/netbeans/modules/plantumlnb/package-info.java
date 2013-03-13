763705770a69348e1f6f2c78eb7fdbffa6672d44
Venkat Akkineni venkatram.akkineni@gmail.com <>
2013-03-12 23:15:32 -0500

    Initial Commit.

---
+++ src/main/java/org/netbeans/modules/plantumlnb/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+@TemplateRegistration(folder = "UML", content = "pumlTemplate.puml")
+package org.netbeans.modules.plantumlnb;
+
+import org.netbeans.api.templates.TemplateRegistration;
