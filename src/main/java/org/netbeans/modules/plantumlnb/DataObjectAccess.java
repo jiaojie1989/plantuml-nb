8e4fe115dc5a9d43358f490439d571e94b9da234
venkat <venkat@venkat>
2013-07-27 21:47:59 -0500

    Added a sample puml file and DataObjectAccess Interface.

---
+++ src/main/java/org/netbeans/modules/plantumlnb/DataObjectAccess.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.netbeans.modules.plantumlnb;
+
+import org.openide.loaders.DataObject;
+
+/**
+ *
+ * @author venkat
+ */
+public interface DataObjectAccess {
+    
+    public DataObject getDataObject();
+    
+}
