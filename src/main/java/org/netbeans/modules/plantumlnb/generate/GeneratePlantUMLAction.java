2372b903d5074f32b7ba9c100e0a94fee7656c5e
Venkat R Akkineni <sriguru@users.sourceforge.net>
2013-11-20 22:13:59 -0600

    -> Added a new context sensitive action to package node. -> Created a service to call PlantUML-Dependency.

---
+++ src/main/java/org/netbeans/modules/plantumlnb/generate/GeneratePlantUMLAction.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.netbeans.modules.plantumlnb.generate;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import net.sourceforge.mazix.cli.exception.CommandLineException;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionRegistration;
+import org.openide.filesystems.FileUtil;
+import org.openide.loaders.DataFolder;
+import org.openide.util.NbBundle.Messages;
+
+/**
+ *
+ * @author Venkat Ram Akkineni
+ */
+@ActionID(
+        category = "Build",
+        id = "org.netbeans.modules.plantumlnb.GeneratePlantUMLAction")
+@ActionRegistration( displayName = "#CTL_OnJavaPackageAction")
+@ActionReferences({
+    @ActionReference(path = "Projects/package/Actions", position = 1400)
+})
+@Messages("CTL_OnJavaPackageAction=Generate Plant UML")
+public final class GeneratePlantUMLAction implements ActionListener {
+    
+    private final List<DataFolder> context;
+    private static final Logger LOG = Logger.getLogger(GeneratePlantUMLAction.class.getName());
+ 
+    public GeneratePlantUMLAction(List<DataFolder> context) {
+        this.context = context;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        ArrayList<File> javaFiles = new ArrayList<>();
+        
+        for(ListIterator contextIter = context.listIterator(); contextIter.hasNext();){
+            DataFolder dataFolder = (DataFolder) contextIter.next();
+//            System.out.println("URI: " + dataFolder.getPrimaryFile().toURI());
+//            System.out.println("URL: " + dataFolder.getPrimaryFile().toURL());
+            File dir = FileUtil.toFile(dataFolder.getPrimaryFile());
+            
+//            File[] javaFiles = dir.listFiles(new FilenameFilter() { 
+//                
+//    	        @Override 
+//                public boolean accept(File dir, String filename) { 
+//                     return filename.endsWith(".java"); 
+//                 }
+//            });
+//            
+//            
+//            System.out.println("Java files count: " + javaFiles.length);
+//            
+//            for(File javaFile : javaFiles) {
+//                System.out.println("Java File: " + javaFile.getAbsolutePath());
+//            }
+//            
+            javaFiles.addAll(list(dir));
+            try {
+                PlantUMLDependencyService.generate(dir, new File("/tmp/" + dir.getName() + ".puml"));
+            } catch(MalformedURLException | CommandLineException | ParseException ex) {
+                LOG.log(Level.INFO, ex.getLocalizedMessage());
+            }
+        }   
+        LOG.log(Level.INFO, "Acting already ...");
+    }
+    
+    public ArrayList<File> list(File file) {
+        File[] children = file.listFiles();
+        ArrayList<File> javaFiles = new ArrayList<>();        
+        /**
+         * If children is not null then 'file' is a directory, recurse.
+         */
+        if(children != null) {
+            for (File child : children) {
+                list(child);
+            }
+        } else {
+            if(file.getName().endsWith(".java")) {
+                javaFiles.add(file);
+                System.out.println(file.getName());
+            }
+        }
+        return javaFiles;
+    }
+    
+}
---
+++ src/main/java/org/netbeans/modules/plantumlnb/generate/PlantUMLDependencyService.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.netbeans.modules.plantumlnb.generate;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.text.ParseException;
+
+import net.sourceforge.mazix.cli.command.CommandLine;
+import net.sourceforge.mazix.cli.command.impl.CommandLineImpl;
+import net.sourceforge.mazix.cli.exception.CommandLineException;
+import net.sourceforge.mazix.cli.program.JavaProgram;
+import net.sourceforge.mazix.cli.program.execution.JavaProgramExecution;
+import net.sourceforge.plantuml.dependency.main.program.PlantUMLDependencyProgram;
+
+/**
+ *
+ * @author Venkat Ram Akkineni
+ */
+public class PlantUMLDependencyService {
+    
+
+    public static void generate(File directory, File outputFile) throws MalformedURLException, CommandLineException, ParseException {
+
+        // Creates the PlantUML Dependency arguments as they would be written in the command line
+//        final CommandLine commandLineArguments = new CommandLineImpl(new String[] {"-o", "plantuml-jdk-1.7.0.45.txt", "-b", "C:\\jdk-1.7.0.45-src", "-e", "**/package-info.java"});
+        final CommandLine commandLineArguments = new CommandLineImpl(new String[] {"-o", outputFile.getAbsolutePath(), "-b", directory.getAbsolutePath(), "-e", "**/package-info.java"});
+
+        // Creates the PlantUML Dependency program instance
+        final JavaProgram plantumlDependencyProgram = new PlantUMLDependencyProgram();
+
+        // Get the PlantUML Dependency program execution instance following the command line arguments, ready to be executed
+        final JavaProgramExecution plantumlDependencyProgramExecution = plantumlDependencyProgram.parseCommandLine(commandLineArguments);
+
+        // Executes the PlantUML Dependency program
+        plantumlDependencyProgramExecution.execute();
+    }
+    
+    
+}
