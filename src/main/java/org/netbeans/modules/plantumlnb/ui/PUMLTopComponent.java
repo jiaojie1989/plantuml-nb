229c4983743182ac3aee32d666dea973b0ac1b8e
Venkat R Akkineni <venkatram.akkineni@gmail.com>
2017-03-30 00:08:08 -0500

    Revert "Merging changes from master_parserJavaToPlant by guglesf."
    This reverts commit 01b948ae030f2443e8e9f918e3713954ad96dfa7.

--- src/main/java/org/netbeans/modules/plantumlnb/ui/PUMLTopComponent.java
+++ src/main/java/org/netbeans/modules/plantumlnb/ui/PUMLTopComponent.java
@@ -24,7 +24,7 @@
 package org.netbeans.modules.plantumlnb.ui;
 
 import java.awt.image.BufferedImage;
-//import java.awt.peer.ComponentPeer;
+import java.awt.peer.ComponentPeer;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.IOException;
@@ -547,7 +547,7 @@ public final class PUMLTopComponent extends TopComponent implements Serializable
             if(evt.getPropertyName().toLowerCase().equals(TopComponent.Registry.PROP_OPENED) 
                     && evt.getNewValue() instanceof TopComponent){
                 TopComponent tc = (TopComponent) evt.getNewValue();
-//                ComponentPeer cp = tc.getPeer();                
+                ComponentPeer cp = tc.getPeer();                
 //                String displayName = ((Node[]) evt.getNewValue())[0].getDisplayName();            
 
 //                if(displayName.toLowerCase().endsWith("puml")){
