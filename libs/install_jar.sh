69d54e49a5a7127de9859bc436243175fb721e6f
Matthias Bläsing <mblaesing@doppel-helix.eu>
2021-04-02 22:09:37 +0200

    Cleanup lexer generation and repositry
    - remove obsolte binaries
    - remove duplicate grammar files
    - remove bogus license statement in grammar file (that belongs to a
      java 1.5 grammer) and replace it with the common license of the whole
      modules
    - generate the lexer at build time

--- libs/install_jar.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mvn install:install-file -Dfile=plantuml-dependency-1.2.0/plantuml-dependency-1.2.0-jar-with-dependencies.jar -DgroupId=net.sourceforge.plantuml-dependency -DartifactId=plantuml-dependency -Dversion=1.2.0 -Dpackaging=jar
